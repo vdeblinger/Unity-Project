b079dc5ecaffd3b5ce24a9ce30786cdef4a84a53
MarcHernX15 <drstein21@live.com>
2024-07-14 16:47:04 -0500

    PhysicsScene Update
    Fully playable PONG in Physics Scene.
    
    Logic for paddles, ball, player goal, player score, reset positions have been added.

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -7,9 +7,14 @@ public class Paddle : MonoBehaviour
     public bool isPlayer1;
     public float speed;
     public Rigidbody2D rb;
+    public Vector3 startPosition;
 
     private float movement;
 
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
     private void Update()
     {
         if (isPlayer1)
@@ -22,5 +27,11 @@ public class Paddle : MonoBehaviour
         }
 
         rb.velocity = new Vector2(rb.velocity.x, movement * speed);
+    }
+
+    public void Reset()
+    {
+        rb.velocity = Vector2.zero;
+        transform.position = startPosition;
     }
 }
